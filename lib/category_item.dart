1f1e210a6cdd0502ee5b3707c08db4e0dfdde7b8
AmanPant777 <footballeramanpt65@gmail.com>
2021-10-17 17:08:02 +0530

    navigation to meals screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 class CategoryItems extends StatelessWidget {
   final String title;
   final Color color;
 
+  void selectedCategory(context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (_) => CategoryMealsScreen()));
+  }
+
   CategoryItems(this.title, this.color);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.7)
-          ,color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-          ),
-          borderRadius: BorderRadius.circular(20)
-          ),
+    return InkWell(
+      onTap: () => selectedCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(20)),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
-
 void main() {
   runApp(MyApp());
 }
@@ -10,10 +9,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+          body1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          body2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51,1)
+          ),
+          title: TextStyle(
+            fontSize: 30,
+            color: Colors.white,
+            fontWeight:FontWeight.bold
+          )
+        )
       ),
       home: CategorieScreen(),
     );
   }
 }
-
