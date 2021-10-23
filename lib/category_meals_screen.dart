ba68f47e84b25fa457d885087c9590f09d82118c
AmanPant777 <footballeramanpt65@gmail.com>
2021-10-23 21:02:28 +0530

    add meal item

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/mealItem.dart';
+import 'dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -9,16 +11,23 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     final categoryId = routeArgs['id'];
     final categoryTitle = routeArgs['title'] as String;
+    final categoryMeals = DUMMY_MEALS.where((meals) {
+      return meals.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
-        appBar: AppBar(
-          title: Text(categoryTitle),
-        ),
-        body: ListView.builder(
-          itemBuilder:(ctx,index){
-            
-          },
-          itemCount: ,
-          )
-        );
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(
+        itemBuilder: (item, index) {
+          return MealItem(title: categoryMeals[index].title, 
+          imageUrl: categoryMeals[index].imageUrl, 
+          duration: categoryMeals[index].duration ,
+          complexity: categoryMeals[index].complexity, 
+          affordability: categoryMeals[index].affordability);
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
   }
 }
