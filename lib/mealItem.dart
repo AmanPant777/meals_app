f37cc8bd4621ad0d9c64eec2c365bf3d019f457e
AmanPant777 <footballeramanpt65@gmail.com>
2021-10-24 15:52:19 +0530

    add text and pricey and duration

--- lib/mealItem.dart
+++ lib/mealItem.dart
@@ -10,10 +10,42 @@ class MealItem extends StatelessWidget {
 
   MealItem(
       {required this.title,
-     required this.imageUrl,
-     required this.duration,
-     required this.complexity,
-     required this.affordability});
+      required this.imageUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability});
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordablityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'Unknown';
+    }
+  }
 
   void selectItem() {}
   @override
@@ -29,14 +61,65 @@ class MealItem extends StatelessWidget {
             Stack(
               children: [
                 ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15)),
-                    child: Image.network(imageUrl,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(
+                    imageUrl,
                     height: 250,
                     width: double.infinity,
-                    fit: BoxFit.contain,))
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                Positioned(
+                    bottom: 10,
+                    right: 1,
+                    child: Container(
+                      color: Colors.black54,
+                      width: 300,
+                      padding: EdgeInsets.all(15),
+                      child: Text(
+                        title,
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                        softWrap: true,
+                        overflow: TextOverflow.fade,
+                      ),
+                    ))
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment:MainAxisAlignment.spaceAround,
+                children: [
+                Row(
+                  children: [
+                    Icon(Icons.schedule),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('$duration')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Icon(Icons.work),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('$complexityText')
+                  ],
+                ),
+                 Row(
+                  children: [
+                    Icon(Icons.attach_money),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('$affordablityText')
+                  ],
+                ),
+              ]),
             )
           ],
         ),
